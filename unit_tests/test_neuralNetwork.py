4cbdbcab92d1adc7e544915c90f0eb7e8da16637
HvE <hannes.von.essen@gmail.com>
2016-06-18 00:24:35 +0200

    Calculate difference for biases and weights between old and new version

--- unit_tests/test_neuralNetwork.py
+++ unit_tests/test_neuralNetwork.py
@@ -23,16 +23,21 @@ class TestNeuralNetwork2(TestCase):
     def check_networks_equal(self):
         biases1 = [None] + [bias.reshape((len(bias),1)) for bias in self.neural_network.biases[1:]]
         biases2 = self.neural_network_2.biases
-        for bias1, bias2 in zip(biases1[1:], biases2[1:]):
-            for i in range(len(bias1)):
-                self.failUnlessAlmostEqual(bias1[i], bias2[i])
 
         weights1 = self.neural_network.weights
         weights2 = self.neural_network_2.weights
-        # weights2[2][0,0] = 300
+
+        diff_b = [biases2 - biases1 for biases2, biases1 in
+                  zip(biases2[1:], biases1[1:])]
+        diff_w = [weights2 - weights1 for weights2, weights1 in
+                  zip(weights2[1:], weights1[1:])]
+        print "!"
+        for bias1, bias2 in zip(biases1[1:], biases2[1:]):
+            for i in range(len(bias1)):
+                self.failUnlessAlmostEqual(bias1[i], bias2[i])
         for w1, w2 in zip(weights1[1:], weights2[1:]):
             for weight1, weight2 in zip(np.nditer(w1, order='C'), np.nditer(w2, order='C')):
-                self.failUnlessAlmostEqual(weight1, weight2, places=1)
+                self.failUnlessAlmostEqual(weight1, weight2)
 
 
     def test_update_mini_batch(self):
